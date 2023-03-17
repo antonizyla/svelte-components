caae92c5504ddbf2bce09e93478b54a693292cca
Antoni Zyla <zyla.antoni@gmail.com>
2023-03-17 17:09:28 +0000

    tailwind fully setup and accordion fixed

---
+++ src/stories/Accordion/Accordion.stories.ts
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import Accordion from './Accordion.svelte';
+
+const meta = {
+    title: 'Accordion',
+    component: Accordion,
+    tags: ['autodocs'],
+    argTypes: {
+        backgroundColor: { control: 'text' },
+    },
+} satisfies Meta<Accordion>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+    args: {
+        accordionItems: [
+            {
+                id: '1',
+                title: 'Accordion Item 1',
+                content: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+                open: true,
+            },
+            {
+                id: '2',
+                title: 'Accordion Item 2',
+                content: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+                open: false,
+            },
+            {
+                id: '3',
+                title: 'Accordion Item 3',
+                content: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+                open: false,
+            },
+            {
+                id: '4',
+                title: 'Accordion Item 4',
+                content: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.',
+                open: false,
+
+            }
+        ],
+        backgroundColor: 'red',
+    },
+}
---
+++ src/stories/Accordion/AccordionItem.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	export let title: string;
+	export let content: string;
+	export let open = false;
+
+	function toggle_closed(): void {
+		open = !open;
+	}
+	//  import IoIosArrowDown from 'svelte-icons/io/IoIosArrowDown.svelte'
+	//import IoIosArrowUp from 'svelte-icons/io/IoIosArrowUp.svelte'
+	import { fade } from 'svelte/transition';
+</script>
+
+<div class="">
+	<button
+		on:click={() => toggle_closed()}
+		class={['p-4 w-full flex flex-row justify-between'].join(' ')}
+	>
+		<div class="">{title}</div>
+		<div class="h-[32px] w-[32px] text-gray-800">
+			{#if open}
+				<div in:fade={{ duration: 500 }}>
+					<!--               <IoIosArrowUp></IoIosArrowUp>
+            -->
+					close
+				</div>
+			{:else}
+				<div in:fade={{ duration: 500 }}>
+					<!--
+               <IoIosArrowDown></IoIosArrowDown>
+                --> open
+				</div>
+			{/if}
+		</div>
+	</button>
+	{#if open}
+		<div class="pb-6 px-6  ">{content}</div>
+	{/if}
+</div>

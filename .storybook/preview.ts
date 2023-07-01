ab5cb8eaf6e1464ead16d0d1fcb963f714c78fb6
Antoni Zyla <zyla.antoni@gmail.com>
2023-07-01 23:19:51 +0100

    migrated to svelte 4

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,5 @@
 import type { Preview } from '@storybook/svelte';
 
-import '../src/app.css';
-
 const preview: Preview = {
 	parameters: {
 		actions: { argTypesRegex: '^on[A-Z].*' },
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { imagetools } from 'vite-imagetools';
-
-import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit(), imagetools()],
-	resolve: {
-		alias: {
-			$src: path.resolve('./src'),
-			$images: path.resolve('./images')
-		}
-	}
+	plugins: [sveltekit()]
 });
